acce47ec5fe442450b0bb66d9045082fe6f1dc70
Raymond Pan <raymondpan2022@gmail.com>
2024-07-23 01:44:50 -0400

    fixes

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -5,7 +5,7 @@ import { TicketCreatedListener } from './events/listeners/ticket-created-listene
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
-  console.log('Starting up....');
+  console.log('orders service starting....');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
