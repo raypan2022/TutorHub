e5d2040021466f3f81eb15a8b4563d6ee19eb3a3
Raymond Pan <raymondpan2022@gmail.com>
2024-07-22 22:29:21 -0400

    fixed

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'www.tutorhubapp.ca',
+        'https://www.tutorhubapp.ca',
       headers: req.headers,
     });
   } else {
